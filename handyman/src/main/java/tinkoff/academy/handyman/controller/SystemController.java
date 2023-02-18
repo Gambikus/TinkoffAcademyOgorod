a6b6672e8ea41c1fcbe895ee14c2328ab4e751f6
Vasya Veretin <ver4vas@ya.ru>
2023-02-18 21:08:16 +0300

    config for actuator and system endpoints for handyman

--- handyman/src/main/java/tinkoff/academy/handyman/controller/SystemController.java
+++ handyman/src/main/java/tinkoff/academy/handyman/controller/SystemController.java
@@ -1,2 +1,24 @@
-package tinkoff.academy.handyman.controller;public class SystemController {
+package tinkoff.academy.handyman.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tinkoff.academy.handyman.response.SystemReadinessResponse;
+
+@RestController
+@RequestMapping("/system")
+public class SystemController {
+
+    @GetMapping(value = "/liveness")
+    public ResponseEntity<?> getLivenessStatus() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/readiness", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<SystemReadinessResponse> getReadinessStatus() {
+        return new ResponseEntity<>(new SystemReadinessResponse("OK"), HttpStatus.OK);
+    }
 }
---
+++ handyman/src/main/java/tinkoff/academy/handyman/response/SystemReadinessResponse.java
@@ -0,0 +1,8 @@
+package tinkoff.academy.handyman.response;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class SystemReadinessResponse {
+    public String HandymanService;
+}
